689b03d2216c265db04b7393c21dd5e4d90faf89
rondarchik <dovgun-viktoriya@mail.ru>
2023-04-10 01:10:11 +0300

    tables in dev but not in prod (test - done)

--- Lab3/lab_3.sql
+++ Lab3/lab_3.sql
@@ -9,6 +9,47 @@
 -- соответствии с очередностью их возможного создания в схеме prod 
 -- (необходимо учитывать foreign key в схеме). 
 -- В случае закольцованных связей выводить соответствующее сообщение
+CREATE OR REPLACE PROCEDURE compare_schemas(
+    dev_schema_name IN VARCHAR2,
+    prod_schema_name IN VARCHAR2
+)
+IS 
+    tables_count NUMBER;
+    is_table_exists BOOLEAN;
+BEGIN
+    -- get number of tables in Dev-schema
+    SELECT COUNT(*) INTO tables_count FROM ALL_TABLES WHERE OWNER = dev_schema_name;
 
+    -- if there are no tables in Dev-schema -> STOP
+    IF tables_count = 0 THEN
+        RAISE_APPLICATION_ERROR(-20002, 'Schema ' || dev_schema_name || ' does not contain tables.');
+    END IF;
 
+     -- get list of tables in dev-schema
+    FOR dev_table IN (SELECT table_name FROM all_tables WHERE owner = dev_schema_name)
+    LOOP
+        is_table_exists := FALSE;
 
+        -- table is exists in prod-schema?
+        FOR prod_table IN (SELECT table_name FROM all_tables WHERE owner = prod_schema_name)
+        LOOP
+            IF dev_table.table_name = prod_table.table_name THEN
+                is_table_exists := TRUE;
+                EXIT;
+            END IF;
+        END LOOP;
+
+        -- tables output (if there are)
+        IF NOT is_table_exists THEN
+            dbms_output.put_line('Table ' || dev_table.table_name || ' does not exist in Prod schema');
+        -- ELSE
+        --     dbms_output.put_line('All OK');
+        END IF;
+
+    END LOOP;
+
+    EXCEPTION
+        WHEN OTHERS THEN
+            dbms_output.put_line('ERROR | ' || SQLERRM);
+
+END compare_schemas;
--- Lab3/test.sql
+++ Lab3/test.sql
@@ -1,10 +1,44 @@
+SET SERVEROUTPUT ON;
+
 -- Create users
--- DROP USER Prod CASCADE;
--- DROP USER Dev CASCADE;
+ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
 
--- CREATE USER Prod IDENTIFIED BY password;
--- CREATE USER Dev IDENTIFIED BY password;
+DROP USER Prod CASCADE;
+DROP USER Dev CASCADE;
 
--- GRANT CONNECT, RESOURCE TO Prod;
--- GRANT CONNECT, RESOURCE TO Dev;
+CREATE USER Prod IDENTIFIED BY password;
+CREATE USER Dev IDENTIFIED BY password;
 
+GRANT CONNECT, RESOURCE TO Prod;
+GRANT CONNECT, RESOURCE TO Dev;
+
+connect dev/password@localhost/xepdb1;
+connect prod/password@localhost/xepdb1;
+connect sys/password@localhost/xepdb1 as sysdba;
+
+-- Test 1: empty dev&prod schemas
+BEGIN
+    COMPARE_SCHEMAS('DEV', 'PROD');
+END;
+
+-- Test 2: add table to Dev
+CREATE TABLE Dev.Table1 (id NUMBER);
+
+BEGIN
+    COMPARE_SCHEMAS('DEV', 'PROD');
+END;
+
+-- Test 3: add same table to Prod
+CREATE TABLE Prod.Table1 (id NUMBER);
+
+BEGIN
+    COMPARE_SCHEMAS('DEV', 'PROD');
+END;
+
+-- Test 4: add 2 new tables into Dev
+CREATE TABLE Dev.Table2 (id NUMBER);
+CREATE TABLE Dev.Table3 (id NUMBER);
+
+BEGIN
+    COMPARE_SCHEMAS('DEV', 'PROD');
+END;
